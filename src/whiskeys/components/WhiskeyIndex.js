cb329c0be24505adca530eef4e097d380830e051
peterchang2 <peterchang664@gmail.com>
2019-01-22 16:03:48 -0500

    code refactor and clean up

--- src/whiskeys/components/WhiskeyIndex.js
+++ src/whiskeys/components/WhiskeyIndex.js
@@ -7,7 +7,6 @@ import './whiskeyIndex.scss'
 class WhiskeyIndex extends Component {
   constructor (props) {
     super(props)
-    // console.log(props)
     this.state = {
       whiskeys: [],
       user: props.user
