c3f2827913aee32afa5666713e488040a73c7357
peterchang2 <peterchang664@gmail.com>
2019-01-22 11:38:07 -0500

    started FavoriteList component

---
+++ src/whiskeys/components/FavoriteList.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import { Link, withRouter } from 'react-router-dom'
+
+
+class FavoriteList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state= {
+      
+    }
+  }
+
+
+}
+
+export default withRouter(FavoriteList)
