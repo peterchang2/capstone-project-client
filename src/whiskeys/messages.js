4f43bde15a44025990a7f7ee33afac7b81ad9844
peterchang2 <peterchang664@gmail.com>
2019-01-24 13:35:10 -0500

    Flash message file for Whiskey components

---
+++ src/whiskeys/messages.js
@@ -0,0 +1,9 @@
+export default {
+  user_scoreUpdate: 'User Score Updated',
+  deleteSuccess: 'Item Deleted',
+  addToFavoriteSuccess: 'Added Whiskey to Favorites',
+  addToWishListSuccess: 'Added Whiskey to Wish List',
+  getAllWhiskeysSuccess: 'All Items Retrieved',
+  getAllFavoritesSuccess: 'All Items In Favorites Retrieved',
+  getAllWishListSuccess: 'All Items in Wish List Retrieved'
+}
